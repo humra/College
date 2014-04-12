583353418799fac1edc1b26f4130659c70d466b2
humra7 <matija.huremovic@gmail.com>
2014-04-12 16:28:13 +0200

    Translated

--- Liste/DinamickiStog/program.cpp
+++ Liste/DinamickiStog/program.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 int main() {
 
-	stack brojevi;
+	stack numbers;
 	ELTYPE x;
 
 	//Is it empty?
-	cout << brojevi.is_empty() << endl;
+	cout << numbers.is_empty() << endl;
 
 	//Filling up stack
-	brojevi.push(22);
-	brojevi.push(-100);
-	brojevi.push(300);
-	brojevi.push(41);
+	numbers.push(22);
+	numbers.push(-100);
+	numbers.push(300);
+	numbers.push(41);
 
 	//Is it empty?
-	cout << brojevi.is_empty() << endl;
+	cout << numbers.is_empty() << endl;
 
 	//What is on top?
-	if (brojevi.top(x)) {
+	if (numbers.top(x)) {
 		cout << "Element na vrhu je: " << x << endl;
 	}
 	else {
@@ -30,8 +30,8 @@ int main() {
 	}
 
 	//Pop and print elements.
-	while (!brojevi.is_empty()) {
-		if (brojevi.pop(x)) {
+	while (!numbers.is_empty()) {
+		if (numbers.pop(x)) {
 			cout << "Skinuo: " << x << endl;
 		}
 		else {
