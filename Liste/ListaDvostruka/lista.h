c8a3d799a47a7293e12e832392f7b5b715bf0edf
humra7 <matija.huremovic@gmail.com>
2014-04-15 08:47:08 +0200

    Added a header

---
+++ Liste/ListaDvostruka/lista.h
@@ -0,0 +1,40 @@
+#ifndef _LISTA_H_
+#define _LISTA_H_
+
+#include <string>
+
+using namespace std;
+
+typedef string ELTYPE;
+
+struct cvor;
+
+typedef cvor* POSITION;
+
+struct cvor
+{
+	ELTYPE element;
+	POSITION previous;
+	POSITION next;
+};
+
+class lista
+{
+private:
+	POSITION _head; //first before the start of the list
+	POSITION _tail; // first after the end of the list
+
+public:
+	lista();
+	POSITION end();
+	POSITION first();
+	bool insert(ELTYPE element, POSITION pos);
+	bool read(POSITION pos, ELTYPE& element);
+	bool remove(POSITON pos);
+	POSITION find(ELTYPE element);
+	POSITION empty();
+	POSITION next(POSITION pos);
+	POSITION prev(POSITION pos);
+};
+
+#endif
