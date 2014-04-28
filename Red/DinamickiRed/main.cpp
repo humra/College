fd5b119dd4e851c1eb466c64e645d6d1ac553723
humra7 <matija.huremovic@gmail.com>
2014-04-28 19:37:15 +0200

    Implemented dynamic queue

---
+++ Red/DinamickiRed/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "dinamicki_red.h"
+
+using namespace std;
+
+int main() {
+    
+    queue numbers;
+
+    numbers.enqueue(4);
+    numbers.enqueue(7);
+    numbers.enqueue(42);
+    numbers.enqueue(12);
+
+    int temp = 999;
+    numbers.dequeue(temp);
+    
+    return 0;
+}
+
