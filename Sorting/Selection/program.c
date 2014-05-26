d47af2565257b5af30988c08f528246923ef4194
humra7 <matija.huremovic@gmail.com>
2014-05-26 21:33:18 +0200

    Selection sort in C

---
+++ Sorting/Selection/program.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void sort(int* data, int n) {
+	
+	int j, minindex, x, temp;
+	for(j = 0; j < n - 1; j++) {
+		minindex = j;
+		for(x = j + 1; x < n; x++) {
+			if(data[x] < data[minindex]) {
+				minindex = x;
+			}
+		}
+		//if(minindex != j) {
+			temp = data[j];
+			data[j] = data[minindex];
+			data[minindex] = temp;
+		//}
+	}
+}
+
+int main() {
+
+	int data[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	
+	sort(data, 10);
+
+	int i;
+	for(i = 0; i < 10; i++) {
+		printf("%d ", data[i]);
+	}
+	printf("\n");	
+
+	return 0;
+}
