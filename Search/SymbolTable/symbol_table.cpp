e14cd6124d7383d1da932147c0fa237a18e930fa
humra7 <matija.huremovic@gmail.com>
2014-06-04 17:38:16 +0200

    Fixed indexing

--- Search/SymbolTable/symbol_table.cpp
+++ Search/SymbolTable/symbol_table.cpp
@@ -8,34 +8,34 @@ symbol_table::symbol_table() {
 }
 
 bool symbol_table::insert(unsigned int key, ELTYPE person) {
-	if(_people[key - 1].ID == key) {
+	if(_people[key].ID == key) {
 		cout << "The element with ID of " << key << " alredy exists!" << endl;
 		return 0;
 	}
 
-	_people[key - 1] = person;
+	_people[key] = person;
 	return true;
 }
 
 bool symbol_table::remove(unsigned int key) {
-	if(_people[key - 1].ID != key) {
+	if(_people[key].ID != key) {
 		cout << "The element with ID " << key << " does not exist!" << endl;
 		return false;
 	}
 
-	_people[key - 1].ID = 0;
-	_people[key - 1].name = "";
-	_people[key - 1].lastName = "";
-	_people[key - 1].height = 0;
-	_people[key - 1].pay = 0;
+	_people[key].ID = 0;
+	_people[key].name = "";
+	_people[key].lastName = "";
+	_people[key].height = 0;
+	_people[key].pay = 0;
 	return true;
 }
 
 ELTYPE* symbol_table::find(unsigned int key) {
-	if(_people[key - 1].ID != key) {
+	if(_people[key].ID != key) {
 		return nullptr;
 	}
 
-	ELTYPE* pperson = &_people[key - 1];
+	ELTYPE* pperson = &_people[key];
 	return pperson;
 }
