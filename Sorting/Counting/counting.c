b382a9cb0642f34cfdcf62ec6a2f0bd7a893dae0
humra7 <matija.huremovic@gmail.com>
2014-06-07 19:03:30 +0200

    Removed testing parts of functions

--- Sorting/Counting/counting.c
+++ Sorting/Counting/counting.c
@@ -9,40 +9,28 @@ void counting_sort(int* data, int n) {
 			max = data[i];
 		}
 	}
-	//printf("%d\n", max);
 
 	int* countingArray = (int*) calloc(max + 1, sizeof(int));
 	int* sorted = (int*) malloc (n);
 	for(i = 0; i < max + 1; i++) {
 		countingArray[i] = 0;
-		//printf("%d ", countingArray[i]);
 	}
-	//putchar('\n');
 
 	int temp;
 	for(i = 0; i < n; i++) {
 		temp = data[i];
 		countingArray[temp]++;
 	}
-	/*for(i = 0; i <= max; i++) {
-		printf("%d ", countingArray[i]);
-	}*/
 
 	for(i = 1; i <= max; i++) {
 		countingArray[i] += countingArray[i - 1];
 	}
-	/*for(i = 0; i <= max; i++) {
-		printf("%d ", countingArray[i]);
-	}*/
 
 	for(i = 0; i < n; i++) {
 		temp = data[i];
 		countingArray[temp]--;
 		sorted[countingArray[temp]] = data[i];
 	}
-	/*for(i = 0; i < n; i++) {
-		printf("%d ", sorted[i]);
-	}*/
 
 	for(i = 0; i < n; i++) {
 		data[i] = sorted[i];
@@ -55,7 +43,7 @@ int main() {
 	int n = sizeof(data) / sizeof(data[0]);
 
 	counting_sort(data, n);
-	//putchar('\n');
+	
 	int i;
 	for(i = 0; i < n; i++) {
 		printf("%d ", data[i]);
