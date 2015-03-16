24b685dcd063742afcb2c258dfe5b166d8d7955d
Matija Huremović <matija.huremovic@gmail.com>
2015-03-16 19:54:50 +0100

    First set of practice tasks

---
+++ RWA/Vježbe01/Kalkulator/Calc.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Calc.aspx.cs" Inherits="Calc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server" method="get" action="Rezultat.aspx">
+    <div>
+    
+        <input id="txtPrviBroj" type="text" name="prvi"/><br />
+        <br />
+        <input id="txtDrugiBroj" type="text" name="drugi"/><br />
+        <br />
+        <select id="slcRacunskaOperacija" name="operacija">
+            <option>+</option>
+            <option>-</option>
+            <option>*</option>
+            <option>/</option>
+        </select><br />
+        <br />
+        <br />
+        <input type="submit" id="btn" value="Izracunaj"/></div>
+    </form>
+</body>
+</html>
---
+++ RWA/Vježbe01/Kalkulator/Rezultat.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Rezultat : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string prvi = Request.QueryString["prvi"];
+        string drugi = Request.QueryString["drugi"];
+        string operacija = Request.QueryString["operacija"];
+
+        float rezultat = 0f;
+        int a = 0;
+        int b = 0;
+
+        if (prvi != "" && drugi != "")
+        {
+            try
+            {
+                a = int.Parse(prvi);
+                b = int.Parse(drugi);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+            switch (operacija)
+            {
+                case "+":
+                    rezultat = a + b;
+                    break;
+                case "-":
+                    rezultat = a - b;
+                    break;
+                case "*":
+                    rezultat = a * b;
+                    break;
+                case "/":
+                    rezultat = a / b;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            Response.Redirect("Calc.aspx");
+        }
+
+        Response.Write(String.Format("<h1>{0} {1} {2} = {3}</h1>", a, operacija, b, rezultat));
+        Response.Write("<a href='Calc.aspx'>Ponovi unos</a>");
+    }
+}
