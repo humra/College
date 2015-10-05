b3de82b4be9d2c5fb38d0c68e535646951b6e0b8
Matija Huremović <matija.huremovic@gmail.com>
2015-10-05 11:30:48 +0200

    Finished task set

---
+++ PPPK/Vjezbe01/Zadatak03/Zadatak03.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Zadatak03.aspx.cs" Inherits="Zadatak03" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>Baza:  </td>
+                <td>
+                    <asp:TextBox ID="tbDBName" runat="server" Width="128px"></asp:TextBox>
+                </td>
+                <td>
+                    <asp:Button ID="btnOpen" runat="server" OnClick="btnOpen_Click" Text="OTVORI" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:ListBox ID="lbLog" runat="server"></asp:ListBox>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ PPPK/Vjezbe01/Zadatak03/Zadatak03.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Zadatak03 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOpen_Click(object sender, EventArgs e)
+    {
+        string dbName = tbDBName.Text;
+
+        SqlConnection con = new SqlConnection();
+        con.StateChange += con_StateChange;
+        con.InfoMessage += con_InfoMessage;
+
+        try
+        {
+            con.ConnectionString = ConfigurationManager.ConnectionStrings[dbName].ConnectionString;
+            con.Open();
+        }
+        catch (SqlException ex)
+        {
+            lbLog.Items.Add(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            lbLog.Items.Add(ex.Message);
+        }
+        finally
+        {
+            con.Dispose();
+        }
+
+    }
+
+    void con_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+    {
+        lbLog.Items.Add("INFO: " + e.Message);
+    }
+
+    void con_StateChange(object sender, System.Data.StateChangeEventArgs e)
+    {
+        lbLog.Items.Add("STATE CHANGE: " + e.CurrentState);
+    }
+}
