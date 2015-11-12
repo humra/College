af9615c278187cf5d5ea8f7027f64440d9bba9ab
Matija Huremović <matija.huremovic@gmail.com>
2015-11-12 19:28:22 +0100

    Upload

--- PPPK/PPPKVjezbe04/src/racunarstvo/pppk/DataBaseReader.java
+++ PPPK/PPPKVjezbe04/src/racunarstvo/pppk/DataBaseReader.java
@@ -3,7 +3,9 @@ package racunarstvo.pppk;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sql.DataSource;
@@ -13,7 +15,9 @@ public class DataBaseReader {
     public static void main(String[] args) {
         DataSource ds = kreirajDataSource();
         
-        ispisiPodatkeOStudentima(ds);
+//        ispisiPodatkeOStudentima(ds);
+//        ispisiKolegije(ds);
+        povecajECTS(ds);
     }
 
     private static DataSource kreirajDataSource() {
@@ -22,7 +26,6 @@ public class DataBaseReader {
         ds.setDatabaseName("PPPK4");
         ds.setUser("Humra");
         ds.setPassword("1234");
-        //ds.setIntegratedSecurity(true);
         ds.setServerName("MATIJA-LAPTOP\\SQLEXPRESS");
         
         return ds;
@@ -31,10 +34,45 @@ public class DataBaseReader {
     private static void ispisiPodatkeOStudentima(DataSource ds) {
         String query = "SELECT * FROM Student";
         
-        String cs = "jdbc:sqlserver://MATIJA-LAPTOP\\SQLEXPRESS;databaseName=PPPK;integratedSecurity=true";
+        try(Connection con = ds.getConnection()) {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            
+            while(rs.next()) {
+                System.out.println(String.format("%s %s %d", rs.getString("Ime"), rs.getString("Prezime"), rs.getInt("IDStudent")));
+            }
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void ispisiKolegije(DataSource ds) {
+        String query = "SELECT * FROM KolegijNastavnik INNER JOIN Nastavnik ON " + 
+                "KolegijNastavnik.IDNastavnik = Nastavnik.IDNastavnik " + 
+                "INNER JOIN Kolegij ON KolegijNastavnik.IDKolegij = Kolegij.IDKolegij";
         
         try(Connection con = ds.getConnection()) {
-            System.out.println("weeeeeeeee");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            
+            while(rs.next()) {
+                System.out.println(String.format("%s - %s %s", rs.getString("Naziv"), rs.getString("Ime"), rs.getString("Prezime")));
+            }
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void povecajECTS(DataSource ds) {
+        String query = "UPDATE Kolegij SET ECTS = ECTS + 1 WHERE Semestar = 2";
+        
+        try(Connection con = ds.getConnection()) {
+            Statement st = con.createStatement();
+            int updateanihRedova = st.executeUpdate(query);
+            
+            System.out.println("Updateano " + updateanihRedova + " kolegija");
         }
         catch (SQLException ex) {
             System.out.println(ex.getMessage());
