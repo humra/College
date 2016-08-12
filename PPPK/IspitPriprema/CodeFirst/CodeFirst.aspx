0afdb5d6794a68da8a53b8bcc6292395b8247491
humra <matija.huremovic@gmail.com>
2016-08-12 17:35:49 +0200

    Entity practice

---
+++ PPPK/IspitPriprema/CodeFirst/CodeFirst.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CodeFirst.aspx.cs" Inherits="CodeFirst.CodeFirst" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:DropDownList ID="ddlDrzava" runat="server" AutoPostBack="True" Height="28px" OnSelectedIndexChanged="ddlDrzava_SelectedIndexChanged" Width="201px">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:ListBox ID="lbGrad" runat="server" Height="223px" Width="247px"></asp:ListBox>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ PPPK/IspitPriprema/CodeFirst/CodeFirst.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeFirst
+{
+    public partial class CodeFirst : System.Web.UI.Page
+    {
+        CodeFirstModel db = new CodeFirstModel();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                napuniDrzave();
+            }
+        }
+
+        private void napuniDrzave()
+        {
+            var drzave = (from d in db.Drzavas select d).ToList();
+
+            foreach(Drzava d in drzave)
+            {
+                ddlDrzava.Items.Add(new ListItem(d.Naziv, d.IDDrzava.ToString()));
+            }
+
+            ddlDrzava.SelectedIndex = 0;
+            napuniGradove();
+        }
+
+        private void napuniGradove()
+        {
+            var gradovi = (from g in db.Grads where g.DrzavaID.ToString().Equals(ddlDrzava.SelectedValue) select g).ToList();
+            lbGrad.Items.Clear();
+
+            foreach(Grad g in gradovi)
+            {
+                lbGrad.Items.Add(new ListItem(g.Naziv));
+            }
+        }
+
+        protected void ddlDrzava_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            napuniGradove();
+        }
+    }
+}
