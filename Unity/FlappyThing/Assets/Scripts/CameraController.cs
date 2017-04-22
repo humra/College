6c50e360a382feac23b1240a992ba8d7cb3c30dc
Matija Huremović <matija.huremovic@gmail.com>
2017-04-22 12:21:52 +0200

    Totally not flappy bird progress

---
+++ Unity/FlappyThing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private float cameraHorizontalOffset;
+
+	// Use this for initialization
+	void Start () {
+        cameraHorizontalOffset = transform.position.x - player.transform.position.x;
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 calculatedPosition = new Vector3(player.transform.position.x + cameraHorizontalOffset, transform.position.y, -10);
+        transform.position = calculatedPosition;
+    }
+}
---
+++ Unity/FlappyThing/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public GameObject backgroundTile;
+    private float initialOffset = -2.1f;
+    private float nextTilePosition;
+    float tileWidth;
+
+	// Use this for initialization
+	void Start () {
+        tileWidth = backgroundTile.GetComponentInChildren<Renderer>().bounds.size.x - 0.02f;
+        
+        nextTilePosition = initialOffset;
+        for(var i = 0; i < 6; i++)
+        {
+            Instantiate(backgroundTile, new Vector3(nextTilePosition, 0, 0), Quaternion.identity);
+            nextTilePosition += tileWidth;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        collider.transform.position = new Vector3(nextTilePosition, 0, 0);
+        nextTilePosition += tileWidth;
+    }
+}
