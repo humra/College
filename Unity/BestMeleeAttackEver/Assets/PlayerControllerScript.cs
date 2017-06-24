be4f5b725d9b57bfecf0558cf587cba99565fee3
Matija Huremović <matija.huremovic@gmail.com>
2017-06-24 12:33:33 +0200

    Made the best attack animation ever

---
+++ Unity/BestMeleeAttackEver/Assets/PlayerControllerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+
+        transform.Translate(horizontalMovement * speed, 0, 0);
+	}
+}
