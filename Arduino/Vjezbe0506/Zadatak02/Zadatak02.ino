7175d7e6a5ba016204b2b84c2b48ae5f00469716
Matija Huremović <matija.huremovic@gmail.com>
2017-07-12 23:52:10 +0200

    Arduino

---
+++ Arduino/Vjezbe0506/Zadatak02/Zadatak02.ino
@@ -0,0 +1,29 @@
+#include <Drive.h>
+
+const short MAX_SPEED = 255;
+
+Drive drive(4, 5, 9, 2, 3, 8);
+
+int IC_PIN = 11;
+
+double value;
+long startTime;
+
+void setup() {
+  pinMode(IC_PIN, INPUT);
+}
+
+void loop() {
+  value = digitalRead(IC_PIN);
+  
+  if(value == 0) {
+    startTime = millis();
+  }
+
+  if(millis() - startTime < 2000) {
+    drive.driveBackwards();
+  }
+  else {
+    drive.stopDriving();
+  }
+}
