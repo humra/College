b700a14622c3a9af126d68aab0c6bc7141068833
Humra <matija.huremovic@gmail.com>
2019-06-27 22:08:30 +0200

    Initial upload, run and idle animation, basic controller

---
+++ Unity/Cinematic/Assets/Scripts/Runner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Runner : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Rigidbody rb;
+    private float timestamp;
+
+    public Transform[] target;
+
+    private bool moved = true;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        timestamp = Time.time + 3;
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+
+        if(timestamp <= Time.time && moved)
+        {
+            agent.SetDestination(target[0].position);
+            moved = false;
+        }
+    }
+}
